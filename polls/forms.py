06a8e7625d61431d3754f5c5a7b7d88ef2ebd6ca
Artem Petrenko <45360469+VORISHcA@users.noreply.github.com>
2020-04-03 19:17:44 +0300

    Day 1

---
+++ polls/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+from .models import News
+
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = News
+        fields = ('review_text',)
+
+
+class GetForm(forms.ModelForm):
+
+    class Meta:
+        model = News
+        fields = ('review_text',)
